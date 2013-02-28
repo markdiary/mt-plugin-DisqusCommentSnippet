49b1b5156007a992c7b25a54a3a74942988f0253
maRk <mark45u.u4gm2006@gmail.com>
2013-02-28 18:17:10 +0900

    initialize repository

---
+++ DisqusCommentSnippet/lib/DisqusCommentSnippet/Tags.pm
@@ -0,0 +1,44 @@
+package DisqusCommentSnippet::Tags;
+
+use strict;
+use warnings;
+use MT::Template::Context;
+use MT::Entry;
+
+sub _hdlr_disqus {
+    my ($ctx, $args) = @_;
+    my ($permalink, $dqsShortname, $identifier, $disqus) = '';
+    my $e = $ctx->stash('entry')
+         or return $ctx->_no_entry_error();
+    my $at = $ctx->{current_archive_type} || $ctx->{archive_type};
+    return '' if $at =~ /^(Daily|Weekly|Monthly|Category)/;
+    $permalink = $e->permalink;
+    $permalink = MT::Util::strip_index( $permalink, $ctx->stash('blog') ) unless $args->{with_index};
+    $identifier = $at eq 'Page' ? 'webpage: ' . $permalink 
+                                : $e->id .': ' . $permalink;
+
+    my $plugin = MT->component("DisqusCommentSnippet");
+    my $scope = "blog:".$ctx->stash('blog_id');
+    $dqsShortname = $plugin->get_config_value('mdc_dqsShortname',$scope);
+
+    $disqus = <<"EOC";
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    (function(){
+        var disqus_shortname = '$dqsShortname'; // required: replace example with your forum shortname
+        var disqus_url = '$permalink';
+        var disqus_identifier = '$identifier';
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript><p>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></p></noscript>
+<p><a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a></p>
+EOC
+    return $disqus;
+}
+
+1;
---
+++ DisqusCommentSnippet/php/function.mtdisquscommentsnippet.php
@@ -0,0 +1,41 @@
+<?php 
+function smarty_function_mtdisquscommentsnippet($args, &$ctx) {
+
+    $mt = MT::get_instance();
+    $blog_id = 'blog:' . $ctx->stash('blog_id');
+    $blog_config = $mt->db()->fetch_plugin_data('DisqusCommentSnippet','configuration:' . $blog_id);
+    $dsqShortname = $blog_config['mdc_dqsShortname'];
+
+    $e = $ctx->stash('entry');
+    $at = $ctx->stash('current_archive_type');
+    if($at==='Individual'){
+            $permalink = $ctx->tag('EntryPermalink');
+            $eid = $ctx->tag('EntryID');
+            $identifier = $eid .': '. $permalink;
+        } elseif ($at==='Page'){
+            $permalink = $ctx->tag('PagePermalink');
+            $identifier = 'webpage: ' . $permalink;
+        } else { return '';}
+
+
+$disqus = <<<EOC
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    (function(){
+        var disqus_shortname = '$dsqShortname'; // required: replace example with your forum shortname
+        var disqus_url = '$permalink';
+        var disqus_identifier = '$identifier';
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript><p>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></p></noscript>
+<p><a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a></p>
+EOC;
+
+    return ($disqus);
+}
+?>
