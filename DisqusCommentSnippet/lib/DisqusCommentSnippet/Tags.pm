1ac0d0236ebbff6b896bc7e211c71b3009d020be
markdiarycom <mark45u.u4gm2006@gmail.com>
2017-09-28 11:58:39 +0000

    SSL support

--- DisqusCommentSnippet/lib/DisqusCommentSnippet/Tags.pm
+++ DisqusCommentSnippet/lib/DisqusCommentSnippet/Tags.pm
@@ -31,12 +31,12 @@ sub _hdlr_disqus {
 
         /* * * DON'T EDIT BELOW THIS LINE * * */
         var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        dsq.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
     })();
 </script>
-<noscript><p>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></p></noscript>
-<p><a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a></p>
+<noscript><p>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></p></noscript>
+<p><a href="https://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a></p>
 EOC
     return $disqus;
 }
